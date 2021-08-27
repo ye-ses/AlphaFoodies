7fe1f0a41ea0175a3f276d0b88d3e00fc3cae667
ye-ses <nenzhelelen1@gmail.com>
2021-08-27 19:59:12 +0200

    browse menu updated

---
+++ AlphaFoodies/Scripts/Customer.js
@@ -0,0 +1,10 @@
+﻿var popup = document.getElementById("logout");
+var icon = document.getElementById("exit-icon");
+icon.onclick = function(){ 
+    if (popup.style.display === 'none') {
+        popup.style.display = 'block';
+    } else {
+        popup.style.display = 'none';
+    }
+};
+
