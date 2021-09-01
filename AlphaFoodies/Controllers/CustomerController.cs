39aa6218b7feca0b05d85da634de56488bc1f74b
ye-ses <nenzhehlelen1@gmail.com>
2021-09-01 19:53:57 +0200

    not found message

--- AlphaFoodies/Controllers/CustomerController.cs
+++ AlphaFoodies/Controllers/CustomerController.cs
@@ -13,13 +13,23 @@ namespace AlphaFoodies.Controllers
         [HttpGet]
         public ActionResult Index(string text)
         {
-            ViewBag.text = text;
-            ViewBag.found = "Search results for '" + text + "' .";//message to be displayed if searched items were found
-            ViewBag.notfound = "The searched item '" /*message to be displated if no items were found while searching*/
-                + text 
-                + "' is either not available or not sold in this resturant, keep browsing the menu to see available items";
+            ViewBag.text = text; 
             var list = context.MenuItems.Where(x => x.Item_Name.Contains(text) || text == null).ToList();//a list of searched items or all items 
-            return View(list); //returns a customer view and selected items
+            ViewBag.found = "Search results for '" + text + "' .";//message to be displayed if searched items were found 
+            if (list.Count() > 0)
+            {
+                return View(list); //returns a customer view and selected items
+            }
+            else
+            {
+                ViewBag.notfound = "The searched item '" /*message to be displated if no items were found while searching*/
+                + text
+                + "' is either not available or not sold in this resturant, keep browsing the menu to see available items";
+                TempData["notFound"] = "The searched item '" /*message to be displated if no items were found while searching*/
+                + text
+                + "' is either not available or not sold in this resturant, keep browsing the menu to see available items"; 
+                return RedirectToAction("Index");
+            }
         } 
        
     }
